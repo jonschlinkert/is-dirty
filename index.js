c371aa22c15b0b28e8497ea2ea9a7732437e1526
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-17 13:08:51 -0400

    support glob patterns

--- index.js
+++ index.js
@@ -10,20 +10,46 @@
 var fs = require('fs');
 var path = require('path');
 var gitty = require('gitty');
+var mm = require('micromatch');
 
-module.exports = function isDirty(cwd, cb) {
+module.exports = function isDirty(cwd, patterns, cb) {
+  if (typeof patterns === 'function') {
+    cb = patterns;
+    patterns = null;
+  }
+
+  if (typeof cb !== 'function') {
+    throw new TypeError('expected a callback function');
+  }
+
+  if (typeof cwd !== 'string') {
+    cwd = process.cwd();
+  }
+
   var fp = path.resolve(cwd, '.git');
   var repo = gitty(cwd);
 
   fs.stat(fp, function(err, stats) {
-    if (err) return cb(err);
+    if (err) {
+      cb(err);
+      return;
+    }
 
     repo.status(function(err, status) {
-      if (err) return cb(err);
+      if (err) {
+        cb(err);
+        return;
+      }
+
       if (hasFiles(status)) {
-        cb(null, status);
+        if (patterns) {
+          cb(null, getMatches(patterns, status));
+        } else {
+          cb(null, status);
+        }
+
       } else {
-        cb(null, false);
+        cb();
       }
     });
   });
@@ -41,4 +67,25 @@ function hasFiles(status) {
     }
   }
   return false;
+}
+
+function getMatches(patterns, status) {
+  var matches = [];
+  if (status.staged.length) {
+    matches = mm(pluckFiles(status.staged), patterns);
+  }
+  if (status.unstaged.length) {
+    matches = matches.concat(mm(pluckFiles(status.unstaged), patterns));
+  }
+  return matches;
+}
+
+function pluckFiles(arr) {
+  var res = [];
+  var len = arr.length;
+  var idx = -1;
+  while (++idx < len) {
+    res.push(arr[idx].file);
+  }
+  return res;
 }
